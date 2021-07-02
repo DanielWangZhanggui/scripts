098f2ac3083fe8afd824b78639b82de4715bcb90
Daniel Wang <zhaw@microsoft.com>
2021-07-02 23:53:51 +0800

    convert vtt to txt

---
+++ convert_vtt_totxt.py
@@ -0,0 +1,21 @@
+# f = open("New-ways-to-get-started-with-Azure-Database-for-PostgreSQL_en.vtt", "r")
+vttfileName = 'New-ways-to-get-started-with-Azure-Database-for-PostgreSQL_en.vtt'
+textfileName = 'New-ways-to-get-started-with-Azure-Database-for-PostgreSQL_en.txt'
+newlines = []
+bad_words = '-->'
+with open(vttfileName, 'r') as oldfile:
+    for line in oldfile:
+        if not(bad_words in line):
+            newlines.append(line.replace('\r\n',''))
+# print(newlines)
+
+# 
+with open(textfileName, 'w') as newfile:
+    tmpline = ''
+    for newline in newlines:
+        if not(newline.endswith('.')):
+            tmpline = tmpline + newline
+        else:
+            tmpline = tmpline + newline + '\r\n'
+            newfile.write(tmpline)
+            tmpline = ''
